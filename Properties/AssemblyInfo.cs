93c2b42cfcadb091b6223924fcfda51c75d9d855
L33T <worstping.git@gmail.com>
2015-02-20 23:59:40 +0200

    CommonEx: Uploading Structure

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LeagueSharp.CommonEx")]
+[assembly: AssemblyDescription("LeagueSharp.CommonEx Library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("LeagueSharp")]
+[assembly: AssemblyProduct("LeagueSharp.CommonEx")]
+[assembly: AssemblyCopyright("Copyright © LeagueSharp 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("bcbf2079-a871-42de-8b82-8912d1ddfb61")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
