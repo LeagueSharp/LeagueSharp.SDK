a624179e300363822c66c62b115250df5e4be92f
L33T <worstping.git@gmail.com>
2015-02-21 03:12:01 +0200

    Extensions\Unit: Adding Unit class.

--- Core/Extensions/SharpDX/Vector3.cs
+++ Core/Extensions/SharpDX/Vector3.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿#region
+
+using System;
 using System.Collections.Generic;
+
+#endregion
 
 namespace LeagueSharp.CommonEx.Core.Extensions.SharpDX
 {
---
+++ Core/Extensions/Unit.cs
@@ -0,0 +1,53 @@
+﻿#region
+
+using LeagueSharp.CommonEx.Core.Extensions.SharpDX;
+using Vector2 = SharpDX.Vector2;
+using Vector3 = SharpDX.Vector3;
+
+#endregion
+
+namespace LeagueSharp.CommonEx.Core.Extensions
+{
+    public static class Unit
+    {
+        #region IsValid
+
+        /// <summary>
+        ///     Checks if the Unit is valid.
+        /// </summary>
+        /// <param name="base">Unit (Obj_AI_Base)</param>
+        /// <returns>Boolean</returns>
+        public static bool IsValid(this Obj_AI_Base @base)
+        {
+            return @base != null && @base.IsValid;
+        }
+
+        /// <summary>
+        ///     Checks if the target unit is valid
+        /// </summary>
+        /// <param name="unit">Unit</param>
+        /// <param name="range">Range</param>
+        /// <param name="from">Check From</param>
+        /// <returns>Boolean</returns>
+        public static bool IsValidTarget(this AttackableUnit unit,
+            float range = float.MaxValue,
+            Vector3 from = new Vector3())
+        {
+            if (unit == null || !unit.IsValid || unit.IsDead || !unit.IsVisible || !unit.IsTargetable ||
+                !unit.IsInvulnerable)
+            {
+                return false;
+            }
+
+            var @base = unit as Obj_AI_Base;
+            var unitPosition = @base != null ? @base.ServerPosition : unit.Position;
+
+            return !(range < float.MaxValue) ||
+                   !(Vector2.DistanceSquared(
+                       (@from.ToVector2().IsValid() ? @from : ObjectManager.Player.ServerPosition).ToVector2(),
+                       unitPosition.ToVector2()) > range * range);
+        }
+
+        #endregion
+    }
+}
