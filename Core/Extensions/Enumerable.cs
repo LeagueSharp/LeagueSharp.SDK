f24367cec84c4feba813c995ccac01320129ad89
Justin <chewymoonleaguesharp@gmail.com>
2015-02-24 22:47:48 -0500

    Added more enumerable extensions

--- Core/Extensions/Enumerable.cs
+++ Core/Extensions/Enumerable.cs
@@ -3,6 +3,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using LeagueSharp.CommonEx.Core.Utils;
 
 #endregion
 
@@ -13,25 +14,159 @@ namespace LeagueSharp.CommonEx.Core.Extensions
     /// </summary>
     public static class Enumerable
     {
+        #region MinOrDefault
+
+        /// <summary>
+        ///     Gets the minimun value of an IEnumerable by the comparer, or returns the default.
+        /// </summary>
+        /// <typeparam name="T">Type of object</typeparam>
+        /// <typeparam name="TR">Type result of comparer</typeparam>
+        /// <param name="container">Container of values to search through</param>
+        /// <param name="comparer">Function to compare the values</param>
+        /// <returns>The minimum of the objects</returns>
+        public static T MinOrDefault<T, TR>(this IEnumerable<T> container, Func<T, TR> comparer) where TR : IComparable
+        {
+            var enumerator = container.GetEnumerator();
+            if (!enumerator.MoveNext())
+            {
+                return default(T);
+            }
+
+            var minElem = enumerator.Current;
+            var minVal = comparer(minElem);
+
+            while (enumerator.MoveNext())
+            {
+                var currVal = comparer(enumerator.Current);
+
+                if (currVal.CompareTo(minVal) >= 0)
+                {
+                    continue;
+                }
+
+                minVal = currVal;
+                minElem = enumerator.Current;
+            }
+
+            return minElem;
+        }
+
+        #endregion
+
+
+        #region MaxOrDefault
+
+        /// <summary>
+        ///     Gets the maximum value of an IEnumerable by the comparer, or returns the default.
+        /// </summary>
+        /// <param name="container">Container of values to search through</param>
+        /// <param name="comparer">Function to compare values</param>
+        /// <typeparam name="T">Type of object</typeparam>
+        /// <typeparam name="TR">Type result of comparer</typeparam>
+        /// <returns>The maximums of the objects</returns>
+        public static T MaxOrDefault<T, TR>(this IEnumerable<T> container, Func<T, TR> comparer) where TR : IComparable
+        {
+            var enumerator = container.GetEnumerator();
+            if (!enumerator.MoveNext())
+                return default(T);
+
+            var maxElem = enumerator.Current;
+            var maxVal = comparer(maxElem);
+
+            while (enumerator.MoveNext())
+            {
+                var currVal = comparer(enumerator.Current);
+
+                if (currVal.CompareTo(maxVal) <= 0)
+                {
+                    continue;
+                }
+
+                maxVal = currVal;
+                maxElem = enumerator.Current;
+            }
+
+            return maxElem;
+        }
+
+        #endregion
+
+
         #region Find
 
         /// <summary>
-        ///     Searches for an element that matches the conditions defined by the specified predicate, and returns the first
-        ///     occurrence within the entire IEnumerable.
+        ///     Finds match in a container of values.
         /// </summary>
+        /// <param name="source">List of all the values</param>
+        /// <param name="match">Method that determines whether the specified object meets the criteria</param>
+        /// <returns>Matches based on the predicate</returns>
         public static TSource Find<TSource>(this IEnumerable<TSource> source, Predicate<TSource> match)
         {
             return (source as List<TSource> ?? source.ToList()).Find(match);
         }
 
+        #endregion
+
+        #region In
         /// <summary>
-        ///     Retrieves all the elements that match the conditions defined by the specified predicate.
+        ///     Determines if a list contains any of the values.
         /// </summary>
-        public static List<TSource> FindAll<TSource>(this IEnumerable<TSource> source, Predicate<TSource> match)
+        /// <param name="source">Container of objects</param>
+        /// <param name="list">Any object that should be in the container</param>
+        /// <typeparam name="T">Type of object to look for</typeparam>
+        /// <returns>If the contianer contains any values.</returns>
+        public static bool In<T>(this T source, params T[] list)
         {
-            return (source as List<TSource> ?? source.ToList()).FindAll(match);
+            return list.Contains(source);
         }
 
         #endregion
+
+        #region ForEach
+
+        /// <summary>
+        ///     Does an action over a list of types.
+        /// </summary>
+        /// <typeparam name="T">Type of value</typeparam>
+        /// <param name="list">List of values</param>
+        /// <param name="action">Function to call foreach value</param>
+        public static void ForEach<T>(this IEnumerable<T> list, Action<T> action)
+        {
+            foreach (var item in list)
+            {
+                action(item);
+            }
+        }
+
+        #endregion
+
+        #region To
+
+        /// <summary>
+        ///     Converts an item to another Type
+        /// </summary>
+        /// <typeparam name="T">Type to convert to</typeparam>
+        /// <param name="object">The object to conver to</param>
+        /// <returns>The converted object</returns>
+        public static T To<T>(this IConvertible @object)
+        {
+            return (T) Convert.ChangeType(@object, typeof(T));
+        }
+
+        #endregion
+
+        #region Logging
+
+        /// <summary>
+        ///     Logs an exception to the console and a file.
+        /// </summary>
+        /// <param name="exception">Exception to log.</param>
+        public static void Log(this Exception exception)
+        {
+            Logging.Write(true)(LogLevel.Error, exception.Message);
+        }
+
+        #endregion
+
     }
 }
