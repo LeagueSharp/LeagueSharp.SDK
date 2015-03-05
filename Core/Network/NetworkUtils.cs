1cc9ae3d92e68d969412f88f93af08ce8e45e5ff
trees-software <treesbot@gmail.com>
2015-03-05 11:39:13 -0500

    Packet Header => Packet Id, revamped Packet Class

--- Core/Network/NetworkUtils.cs
+++ Core/Network/NetworkUtils.cs
@@ -6,24 +6,34 @@
     public static class NetworkUtils
     {
         /// <summary>
-        ///     Gets the header of the packet.
+        ///     Gets the packet id.
         /// </summary>
-        /// <param name="args">Packet arguements from event</param>
-        /// <returns>The header</returns>
-        public static short GetHeader(this GamePacketEventArgs args)
+        /// <param name="args">Packet arguments from event</param>
+        /// <returns>The packet id</returns>
+        public static short GetPacketId(this GamePacketEventArgs args)
         {
-            return (short) ((args.PacketData[1] << 8) | (args.PacketData[0] << 0));
+            return (short)((args.PacketData[1] << 8) | (args.PacketData[0] << 0));
+        }
+
+        /// <summary>
+        ///     Gets the packet id.
+        /// </summary>
+        /// <param name="data">Raw packet data</param>
+        /// <returns>The packet id</returns>
+        public static short GetPacketId(this byte[] data)
+        {
+            return (short)((data[1] << 8) | (data[0] << 0));
         }
 
         /// <summary>
         ///     Checks if the packet type matches another packet type.
         /// </summary>
         /// <param name="args">Packet arguements from event</param>
-        /// <param name="header">Packet Header</param>
-        /// <returns>Whether the packets have the same time</returns>
-        public static bool Equals(this GamePacketEventArgs args, Packet.C2S header)
+        /// <param name="packet">Packet</param>
+        /// <returns>Whether the packets have the same id</returns>
+        public static bool Equals(this GamePacketEventArgs args, Packet packet)
         {
-            return args.GetHeader() == (byte) header;
+            return args.GetPacketId() == packet.Id;
         }
     }
 }
