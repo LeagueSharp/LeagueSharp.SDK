709af6df96425f0469b90cad17928bafd2253ca1
L33T <worstping.git@gmail.com>
2015-03-17 14:58:44 +0200

    Core: Addressing issue #9, uploading a Visual Studio .gitignore and adding back CollisionableObjects.

---
+++ Core/Enumerations/CollisionableObjects.cs
@@ -0,0 +1,35 @@
+﻿#region
+
+using System;
+
+#endregion
+
+namespace LeagueSharp.CommonEx.Core.Enumerations
+{
+    /// <summary>
+    ///     Collisonable Objects Flags
+    /// </summary>
+    [Flags]
+    public enum CollisionableObjects
+    {
+        /// <summary>
+        ///     Minion Collisionable Flag
+        /// </summary>
+        Minions,
+
+        /// <summary>
+        ///     Hero Collisionable Flag
+        /// </summary>
+        Heroes,
+
+        /// <summary>
+        ///     Yasuo-Wall Collisionable Flag
+        /// </summary>
+        YasuoWall,
+
+        /// <summary>
+        ///     Wall Collisionable Flag
+        /// </summary>
+        Walls
+    }
+}
