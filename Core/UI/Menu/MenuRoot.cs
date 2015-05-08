3becfd514970f9f8167ef8e04cdfe6f3afbc5948
TheSaltyWaffle <thesaltywaffle@users.noreply.github.com>
2015-05-08 14:14:28 +0200

    Menu: MenuRoot

---
+++ Core/UI/Menu/MenuRoot.cs
@@ -0,0 +1,18 @@
+﻿namespace LeagueSharp.CommonEx.Core.UI
+{
+    public class MenuRoot : Menu
+    {
+        public MenuRoot(string name, string displayName, string uniqueString = "")
+            : base(name, displayName, uniqueString) {}
+
+        /// <summary>
+        ///     Attaches the menu towards the main menu.
+        /// </summary>
+        /// <returns>Menu Instance</returns>
+        public Menu Attach()
+        {
+            MenuManager.Instance.Add(this);
+            return this;
+        }
+    }
+}
