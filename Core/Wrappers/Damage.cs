27f9e054a03de9edefbd2526476e0d5c27eabdb6
L33T <worstping.git@gmail.com>
2015-07-30 10:56:07 +0300

    Core\IDrawing: Removing namespace and all related files. SDK: Fixing project dot settings with visual studio 2015 to avoid issues with C#6.0 suggestions at the current time.

--- Core/Wrappers/Damage.cs
+++ Core/Wrappers/Damage.cs
@@ -49,7 +49,7 @@ namespace LeagueSharp.SDK.Core.Wrappers
         /// </summary>
         private static readonly IDictionary<string, byte[]> DamageFiles = new Dictionary<string, byte[]>
                                                                               {
-                                                                                  { "5.13.0.323", Resources._5_14_0_334 }
+                                                                                  { "5.14.0.334", Resources._5_14_0_334 }
                                                                               };
 
         /// <summary>
@@ -494,13 +494,11 @@ namespace LeagueSharp.SDK.Core.Wrappers
                 value = 100 / (100 + (magicResist * source.PercentMagicPenetrationMod) - source.FlatMagicPenetrationMod);
             }
 
-            var damage = source.DamageReductionMod(
-                target, 
-                source.PassivePercentMod(target, value) * amount, 
-                DamageType.Magical, 
+            return source.DamageReductionMod(
+                target,
+                source.PassivePercentMod(target, value) * amount,
+                DamageType.Magical,
                 forceDamageModifier) + source.PassiveFlatMod(target);
-
-            return damage;
         }
 
         /// <summary>
@@ -575,14 +573,12 @@ namespace LeagueSharp.SDK.Core.Wrappers
                 value = 100 / (100 + (armor * armorPenetrationPercent) - armorPenetrationFlat);
             }
 
-            var damage = source.DamageReductionMod(
-                target, 
-                source.PassivePercentMod(target, value) * amount, 
-                DamageType.Physical, 
-                forceDamageModifier) + source.PassiveFlatMod(target);
-
             // Take into account the percent passives, flat passives and damage reduction.
-            return damage;
+            return source.DamageReductionMod(
+                target,
+                source.PassivePercentMod(target, value) * amount,
+                DamageType.Physical,
+                forceDamageModifier) + source.PassiveFlatMod(target);
         }
 
         /// <summary>
