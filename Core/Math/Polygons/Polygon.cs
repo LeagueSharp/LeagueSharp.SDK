beea0e4763932539c6a9a5994c6ed3aa452a1fcb
h3h3 <h3h3sharp@gmail.com>
2015-09-15 17:26:03 +0200

    fixed Polygon Z drawing

--- Core/Math/Polygons/Polygon.cs
+++ Core/Math/Polygons/Polygon.cs
@@ -107,8 +107,8 @@ namespace LeagueSharp.SDK.Core.Math.Polygons
             for (var i = 0; i <= this.Points.Count - 1; i++)
             {
                 var nextIndex = (this.Points.Count - 1 == i) ? 0 : (i + 1);
-                var from = Drawing.WorldToScreen(this.Points[i].ToVector3());
-                var to = Drawing.WorldToScreen(this.Points[nextIndex].ToVector3());
+                var from = Drawing.WorldToScreen(this.Points[i].ToVector3(NavMesh.GetHeightForPosition(this.Points[i].X, this.Points[i].Y)));
+                var to = Drawing.WorldToScreen(this.Points[nextIndex].ToVector3(NavMesh.GetHeightForPosition(this.Points[i].X, this.Points[i].Y)));
 
                 Drawing.DrawLine(from[0], from[1], to[0], to[1], width, color);
             }
