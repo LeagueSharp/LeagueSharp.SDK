8d0625a33c8667b40955e7f678d3239aca708d7a
Alex <thinkinginbinary@bk.ru>
2016-06-20 22:32:14 +0300

    added documentation and incremented old loader version

--- Core/Wrappers/Spells/Database/SpellDatabase.cs
+++ Core/Wrappers/Spells/Database/SpellDatabase.cs
@@ -105,6 +105,12 @@ namespace LeagueSharp.SDK
                     .FirstOrDefault(spellData => objectName.Contains(spellData.SourceObjectName));
         }
 
+        /// <summary>
+        ///     Get the first spell on a spellslot (for champions with more than 1 spellslot use method GetAllSpellsOnSpellSlot)
+        /// </summary>
+        /// <param name="slot">The SpellSlot</param>
+        /// <param name="championName">The Champion Name</param>
+        /// <returns></returns>
         public static SpellDatabaseEntry GetBySpellSlot(SpellSlot slot, string championName = "undefined")
         {
             var actualChampionName = championName.Equals("undefined")
@@ -115,6 +121,12 @@ namespace LeagueSharp.SDK
                     spellData => spellData.ChampionName == actualChampionName && spellData.Slot == slot);
         }
 
+        /// <summary>
+        ///     Get all spells corresponding to a spellslot (useful for nidalee, jayce, elise, leesin)
+        /// </summary>
+        /// <param name="slot">The SpellSlot</param>
+        /// <param name="championName">The Champion Name</param>
+        /// <returns></returns>
         public static IEnumerable<SpellDatabaseEntry> GetAllSpellsOnSpellSlot(
             SpellSlot slot,
             string championName = "undefined")
@@ -125,6 +137,12 @@ namespace LeagueSharp.SDK
             return Spells.Where(spellData => spellData.ChampionName == actualChampionName && spellData.Slot == slot);
         }
 
+        /// <summary>
+        ///     Creates a spell from target spellslot
+        /// </summary>
+        /// <param name="slot">The SpellSlot</param>
+        /// <param name="championName">The Champion Name</param>
+        /// <returns></returns>
         public static Spell MakeSpell(this SpellSlot slot, string championName = "undefined")
         {
             var spellData = GetBySpellSlot(slot, championName);
@@ -132,25 +150,20 @@ namespace LeagueSharp.SDK
             if (spellData.ChargedSpellName != "")
             {
                 return new Spell
-                {
-                    ChargedBuffName = spellData.ChargedBuffName,
-                    ChargedMaxRange = spellData.ChargedMaxRange,
-                    ChargedMinRange = spellData.ChargedMinRange,
-                    ChargedSpellName = spellData.ChargedSpellName,
-                    ChargeDuration = spellData.ChargeDuration,
-                    Delay = spellData.Delay,
-                    Range = spellData.Range,
-                    Width =
+                           {
+                               ChargedBuffName = spellData.ChargedBuffName, ChargedMaxRange = spellData.ChargedMaxRange,
+                               ChargedMinRange = spellData.ChargedMinRange, ChargedSpellName = spellData.ChargedSpellName,
+                               ChargeDuration = spellData.ChargeDuration, Delay = spellData.Delay, Range = spellData.Range,
+                               Width =
                                    spellData.Radius > 0 && spellData.Radius < 30000
                                        ? spellData.Radius
                                        : ((spellData.Width > 0 && spellData.Width < 30000) ? spellData.Width : 30000),
-                    Collision =
+                               Collision =
                                    (spellData.CollisionObjects != null
                                     && spellData.CollisionObjects.Any(obj => obj == CollisionableObjects.Minions)),
-                    Speed = spellData.MissileSpeed,
-                    IsChargedSpell = true,
-                    Type = GetSkillshotTypeFromSpellType(spellData.SpellType)
-                };
+                               Speed = spellData.MissileSpeed, IsChargedSpell = true,
+                               Type = GetSkillshotTypeFromSpellType(spellData.SpellType)
+                           };
             }
             // Skillshot:
             if (spellData.CastType.Any(type => type == CastType.Position || type == CastType.Direction))
@@ -189,21 +202,21 @@ namespace LeagueSharp.SDK
                 case SpellType.SkillshotCone:
                     return SkillshotType.SkillshotCone;
                 case SpellType.SkillshotCircle:
-                    return SkillshotType.SkillshotCircle;
+                return SkillshotType.SkillshotCircle;
                 case SpellType.SkillshotLine:
-                    return SkillshotType.SkillshotLine;
+                return SkillshotType.SkillshotLine;
                 case SpellType.SkillshotMissileArc:
-                    return SkillshotType.SkillshotCone;
+                return SkillshotType.SkillshotCone;
                 case SpellType.Position:
-                    return SkillshotType.SkillshotLine;
+                return SkillshotType.SkillshotLine;
                 case SpellType.SkillshotMissileCircle:
-                    return SkillshotType.SkillshotCircle;
+                return SkillshotType.SkillshotCircle;
                 case SpellType.SkillshotMissileLine:
-                    return SkillshotType.SkillshotLine;
+                return SkillshotType.SkillshotLine;
                 case SpellType.SkillshotMissileCone:
-                    return SkillshotType.SkillshotCircle;
+                return SkillshotType.SkillshotCircle;
                 case SpellType.SkillshotRing:
-                    return SkillshotType.SkillshotCircle;
+                return SkillshotType.SkillshotCircle;
             }
             return SkillshotType.SkillshotLine;
         }
